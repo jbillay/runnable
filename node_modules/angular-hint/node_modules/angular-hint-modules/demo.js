d9c52300e6b4caca9f75f5c490d9d766ba0c935e
Jeremy Billay <jbilla@gmail.com>
2015-03-20 16:01:46 +0100

    remove angular-hint

--- node_modules/angular-hint/node_modules/angular-hint-modules/demo.js
+++
@@ -1,6 +0,0 @@
-angular.module('createdAndLoaded',[]);
-angular.module('createdAndLoaded',[]);
-angular.module('loadedWithBadSpelling',[]);
-angular.module('createdButNotLoaded',[]);
-angular.module('testModule',
-  ['ngHintModules','createdAndLoaded','loadedWithBdSpelling']);
